0068f06b79ed5c54e9505e16d7550bff1ee39e38
StellarByteStudios <82361848+StellarByteStudios@users.noreply.github.com>
2022-02-06 15:07:50 +0100

    Den Bot selbst zur Instanz gemacht

--- src/main/java/SlashyMain.java
+++ src/main/java/SlashyMain.java
@@ -1,16 +1,42 @@
 import net.dv8tion.jda.api.OnlineStatus;
-import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
 
 import javax.security.auth.login.LoginException;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static net.dv8tion.jda.api.OnlineStatus.OFFLINE;
 import static net.dv8tion.jda.api.entities.Activity.listening;
 import static secret.Tokenholder.BOT_TOKEN;
 
 public class SlashyMain {
 
-    public static void main(String[] args) throws LoginException {
+    // Connection zum Bot
+    public ShardManager shardManager;
 
+    public static void main(String[] args) {
+        // Wir müssen am Anfang den Bot instanziieren
+        try {
+            new SlashyMain();
+        } catch (LoginException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Konstruktor der eine Instanz des Bots erzeugt
+    public SlashyMain() throws LoginException {
+        this.shardManager = configureShardManager();
+        System.out.println("Bot geht online");
+    }
+
+
+
+
+    // Erstellt den Bot und übergibt ihm das wichtigste was er brauch
+    public ShardManager configureShardManager() throws LoginException {
         // * * * Startup (Konfiguriert den Bot ganz nackig) * * * //
         DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault("");
         builder.setToken(BOT_TOKEN);
@@ -22,6 +48,9 @@ public class SlashyMain {
         builder.setStatus(OnlineStatus.ONLINE);
 
         // Build (Starten des Bots)
-        builder.build();
+        return builder.build();
+    }
+
+
     }
 }
