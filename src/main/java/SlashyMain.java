92ceb576d007f4d779eee1eb3cb805408cb322d6
StellarByteStudios <82361848+StellarByteStudios@users.noreply.github.com>
2022-02-06 14:33:06 +0100

    Apperance (Status)
    Erste Einstellungen wie der Bot auf dem Server dargestellt wird

--- src/main/java/SlashyMain.java
+++ src/main/java/SlashyMain.java
@@ -1,23 +1,27 @@
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
 
 import javax.security.auth.login.LoginException;
 
+import static net.dv8tion.jda.api.entities.Activity.listening;
 import static secret.Tokenholder.BOT_TOKEN;
 
 public class SlashyMain {
 
     public static void main(String[] args) throws LoginException {
 
+        // * * * Startup (Konfiguriert den Bot ganz nackig) * * * //
         DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault("");
         builder.setToken(BOT_TOKEN);
 
+        // * * * Einstellungen * * * //
+        // Was macht er gerate
+        builder.setActivity(listening("deinem Bullshit"));
+        // Onlinestatus
+        builder.setStatus(OnlineStatus.ONLINE);
 
-
-
-
-
-
-
+        // Build (Starten des Bots)
         builder.build();
     }
 }
