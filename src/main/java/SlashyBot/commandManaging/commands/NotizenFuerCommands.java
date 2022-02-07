3c2952e5717980646494eb98a2730985c4372456
StellarByteStudios <82361848+StellarByteStudios@users.noreply.github.com>
2022-02-07 11:43:13 +0100

    Notizen, was es an Commands noch gibt
    Ich bin ein sehr vergesslicher Mensch und schaue gerne nach

---
+++ src/main/java/SlashyBot/commandManaging/commands/NotizenFuerCommands.java
@@ -0,0 +1,31 @@
+package SlashyBot.commandManaging.commands;
+
+public class NotizenFuerCommands {
+    //        // fullCommand[0] ist immer das Prefix
+//        String[] fullCommand = message.split(" ");
+//        String command = fullCommand[1];
+//
+//        // Nimmt alle Kommands auf mit nur sich selbst als parameter
+//        if (fullCommand.length == 2){
+//            // Erste einstellige Commands
+//            if (fullCommand[1].equalsIgnoreCase("moin")){
+//                // Nachricht senden mit Namennennung
+//                channel.sendMessage("Seeervus, mooooin").queue();
+//                channel.sendMessage("Was willst du von mir " + event.getMember().getEffectiveName() + "?").queue();
+//            }
+//            else if(fullCommand[1].equalsIgnoreCase("time")){
+//                // Gibt die Uhrzeit aus und pingt den Verfasser
+//                Calendar calendar = Calendar.getInstance();
+//                SimpleDateFormat date = new SimpleDateFormat("HH:mm:ss");
+//                channel.sendMessage("Guck doch selba auf die Uhr " + event.getMember().getAsMention() + "!").queue();
+//                channel.sendMessage("Aber weils dus bist " + date.format(calendar.getTime())).queue();
+//            }
+//        }
+    // Notizen für später
+//            } else{
+//                if (event.getAuthor().isBot()){
+//                    System.out.println("Der Bot soll sich nicht selber antworten");
+//                } else {
+//                    channel.sendMessage("Lern Deutsch du Hurasones!!").queue();
+//                }
+}
