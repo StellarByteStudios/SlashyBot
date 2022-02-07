e7e3115177e215cb2d254f287540f6aa13945a27
StellarByteStudios <82361848+StellarByteStudios@users.noreply.github.com>
2022-02-07 10:53:52 +0100

    Commands ausgelagert
    Commands werden jetzt von einem CommandManager verarbeitet und können dann einzeln ausgeführt werden.
    
    Folgende Aufgabe: Commands Schreiben und CommandListener entmüllen

---
+++ src/main/java/commandManaging/CommandManager.java
@@ -0,0 +1,39 @@
+package commandManaging;
+
+import commandManaging.commands.ClearCommand;
+import commandManaging.commands.ServerCommand;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.TextChannel;
+
+import java.util.Locale;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static commandManaging.commands.CommandStringConstants.CLEAR;
+
+public class CommandManager {
+
+    // Hier werden alle Commands gespeichert
+    public ConcurrentHashMap<String, ServerCommand> commands;
+
+    // Erzeugt die HashMap und läd die Comments rein
+    public CommandManager(){
+        this.commands = new ConcurrentHashMap<>();
+        loadCommands();
+    }
+
+    private void loadCommands(){
+        commands.put(CLEAR, new ClearCommand());
+    }
+
+
+    public boolean perform(String commandName, Member member, TextChannel channel, Message message){
+
+        ServerCommand command = commands.get(commandName.toLowerCase());
+        if (command == null){
+            return false;
+        }
+        command.performCommand(member, channel, message);
+        return true;
+    }
+}
--- src/main/java/commandManaging/commands/CommandStringConstants.java
+++ src/main/java/commandManaging/commands/CommandStringConstants.java
@@ -2,5 +2,6 @@ package commandManaging.commands;
 
 public class CommandStringConstants {
 
-    public static final String PREFIX = "// ";
+    public static final String PREFIX = "//";
+    public static final String CLEAR = "clear";
 }
